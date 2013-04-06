ae68475b4461a78b9b4d3090fa201f3e0f139c67
Brian Levinstein <blevinstein@gmail.com>
2013-04-06 16:15:36 -0400

    Begin implementing Communicator and Message.

---
+++ lab7/Communicator.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+
+import lejos.nxt.*;
+import lejox.nxt.comm.*;
+
+public class Communicator implements Runnable {
+    private Queue<Message> messageQueue;
+    private NXTConnection connection;
+    private InputStream is;
+    private OutputStream os;
+    private boolean terminateFlag;
+    private boolean connected;
+
+    // Return true if connected
+    public void isConnected() {
+        return connected;
+    }
+
+    // Start listening for messages
+    public void start() {
+        (new Thread(this)).start();
+    }
+
+    // Stop listening for messages
+    public void stop() {
+        terminateFlag = true;
+    }
+
+    // Return true if there is a message in the queue
+    public boolean hasMessage() {
+        return messageQueue.peek() != null;
+    }
+
+    // Retrieve oldest message in queue
+    public Message getMessage() {
+        return messageQueue.remove();
+    }
+
+    public void sendMessage(Message m) {
+        // TODO: send message
+    }
+
+    // Main thread
+    public void run() {
+        StringBuffer stringBuffer = new StringBuffer();
+        byte[] byteBuffer = new byte[1024];
+
+        connection = Bluetooth.waitForConnection();
+        is = connection.openDataInputStream();
+        os = connection.openDataOutputStream();
+        terminateFlag = false;
+
+        while(!terminateFlag) {
+            int bytesRead = is.read(byteBuffer);
+            if(bytesRead > 0) {
+                stringBuffer.append(new String(byteBuffer, 0, bytesRead));
+                // check for }
+                int endChar = stringBuffer.indexOf("}");
+                if(endChar != -1) {
+                    // check for matching {
+                    int startChar = stringBuffer.indexOf("{");
+                    if(startChar != -1 && startChar < endChar) {
+                        // parse the message and add it to the queue
+                        messageQueue.offer(new Message(stringBuffer.substring(startChar, endChar+1)));
+                        stringBuffer.replace(startChar, endChar+1, "");
+                        // clean out beginning of buffer
+                        if(startChar > 0) {
+                            stringBuffer.replace(0, startChar, "");
+                        }
+                    } else {
+                        // delete malformed input from the buffer
+                        stringBuffer.replace(0, endChar+1, "");
+                        // TODO: log message about malformed input
+                    }
+                }
+            }
+        }
+
+        // disconnect
+        is.close();
+        os.close();
+        connection.close();
+    }
+}
---
+++ lab7/Message.java
@@ -0,0 +1,10 @@
+import java.util.*;
+// typedef Message HashMap<String, String>
+public class Message extends HashMap<String, String> {
+    public Message(String s) {
+        // TODO: implement message parsing
+    }
+    public String toString() {
+        // TODO: implement message serialization
+    }
+}
