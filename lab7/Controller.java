eb591608f760d603538431cd77ce0f27f0a61cc9
Bryan Walsh <bpw7xx@virginia.edu>
2013-04-07 19:40:10 -0400

    blahhhh

--- lab7/Controller.java
+++ lab7/Controller.java
@@ -1,6 +1,6 @@
 import java.io.*;
-import lejos.nxt.*;
-import lejos.nxt.comm.*;
+//import lejos.pc.*;
+import lejos.pc.comm.*;
 
 public class Controller {
     Communicator comm;
@@ -9,8 +9,8 @@ public class Controller {
     Telemetry telemetry;
     
     public Controller(MotorPort clawPort,
-    				  MotorPort leftPort,
-    				  MotorPort rightPort,
+    				  MotorPort leftport,
+    				  MotorPort rightport,
     				  SensorPort light,
     				  SensorPort touch,
     				  SensorPort ultra,
@@ -20,8 +20,9 @@ public class Controller {
     	telemetry = new Telemetry(light, touch, ultra, sound);
     }
    
-    public void mainLoop() {
+    public static void mainLoop() {
         comm = new Communicator();
+        motionController = new MotionController();
        
         comm.start();
 
@@ -29,14 +30,56 @@ public class Controller {
             Message message;
             
             if (!comm.hasMessage() && comm.isConnected()) {
-            	//Thread.sleep(20);
+            	Thread.sleep(20);
             	continue;
             }
             if (!comm.isConnected()) {
             	//autopilot
             }
-            if (comm.hasMessage()) {
+            if (comm.hasMesssage()) {
+            	
             	message = comm.getMessage();
+            	
+            	//parse the message
+            	
+               	String command = message.pairs.get(0)[0];
+               	String param = message.pairs.get(0)[1];
+               	
+            	if (command.equals("init")){
+            		
+            	}
+            	else if (command.equals("move")){
+            		int move = Integer.parseInt(param);
+            		movement.setSpeed(move, move);
+            	}
+            	else if (command.equals("turn")){
+            		int degrees = Integer.parseInt(param);
+            		//somehow do something to turn the robot
+            	}
+            	else if (command.equals("claw")){
+            		float heading = Float.parseFloat(param);
+            		
+            	}
+            	else if (command.equals("stop")){
+            		movement.setSpeed(0,0);
+            	}
+            	else if (command.equals("query")) {
+            		// get telemetry data, construct message, comm.send() it
+            		
+            	}
+            	else if (command.equals("quit")){
+            		System.exit(-1);
+            	}
+            	else if (command.equals("ack")){
+            		System.out.println("Waiting for additional commands...");
+            	}
+            	else{
+            		//not a valid command... 
+            	}
+            			
+            	
+            	
+            
             
             }
         }
