a68f92e3de7959258c957fce775d42df00051262
Neal Milstein <neal.milstein@gmail.com>
2013-04-07 17:56:31 -0400

    Reverted Message.java

--- lab7/Message.java
+++ lab7/Message.java
@@ -1,47 +1,74 @@
 import java.util.*;
+import java.util.regex.*;
+import java.util.zip.*;
+
+/*
+ * Message format:
+ * {0x:y&y:z|asdf1234}
+ * 0 is the sequence number (valid values: 0 and 1)
+ * then there is a list of key-value pairs (or just tokens, which are treated as keys corresponding to empty values)
+ * then a checksum
+ */
 
 public class Message {
-    private HashMap<String, String> pairs;
+    private int seqNum;
+    private HashMap<String, String> map;
 
     // construct empty message
     public Message() {
-        pairs = new HashMap<String, String>();
+        map = new HashMap<String, String>();
+    }
+
+    private int checksumOf(String s) {
+        int cs = 0;
+        for(int i=0; i<s.length(); i++)
+            cs += s.charAt(i);
+        return ~cs;
     }
 
     // construct the original message from a string
     public Message(String s) {
-        pairs = new HashMap<String, String>();
-        if(s.charAt(0) != '{' || s.charAt(s.length()-1) != '}')
-            return;
-            // TODO: throw error here?
-        String trimmed = s.substring(1,s.length()-1);
+        map = new HashMap<String, String>();
+        Pattern p = new Pattern("^\{(\d)(.+)\|(.+)\}$");
+        Matcher m = p.matcher(s);
+        if(!m.matches() || m.groupCount() < 3)
+            return; // TODO: error here
+        // get sequence number
+        seqNum = Integer.parseInt(m.group(0));
+        // calculate and compare checksum
+        int checksum = checksumOf(m.group(0) + m.group(1));
+        int messageChecksum = Integer.parseInt(m.group(2));
+        if(checksum != messageChecksum)
+            return; // TODO: error here
+        // parse body
+        String trimmed = m.group(1);
         String[] split = trimmed.split("&");
         for(int i=0; i<split.length; i++) {
             int colon = split[i].indexOf(":");
             if(colon == -1)
-                add(split[i], "");
+                map.put(split[i], "");
             else
-                add(split[i].substring(0,colon), split[i].substring(colon+1));
+                map.put(split[i].substring(0,colon), split[i].substring(colon+1));
         }
     }
 
     // return the underlying HashMap
-    public HashMap<String, String> getPairs() {
-        return pairs;
+    public HashMap<String, String> getMap() {
+        return map;
     }
 
-    // add a key-value pair
-    public void add(String key,String value) {
-        pairs.put(key, value);
+    // return the sequence number
+    public int getSeqNum() {
+        return seqNum;
     }
 
     // serialize message for transmission
     public String toString() {
         StringBuffer b = new StringBuffer("{");
-        Iterator<String> keys = pairs.keySet().iterator();
+        Iterator<String> keys = map.keySet().iterator();
         while(keys.hasNext()) {
             String k = keys.next();
-            String v = pairs.get(k);
+            String v = map.get(k);
             if(v.length() > 0) {
                 b.append(k);
                 b.append(':');
