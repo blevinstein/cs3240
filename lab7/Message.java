eb504e6300c0936f977d4d2eaee4e8c26e363c6a
Neal Milstein <neal.milstein@gmail.com>
2013-04-07 18:30:27 -0400

    Changed Message.java to use ArrayList

--- lab7/Message.java
+++ lab7/Message.java
@@ -12,11 +12,12 @@ import java.util.zip.*;
 
 public class Message {
     private int seqNum;
-    private HashMap<String, String> map;
+    private ArrayList<String[]> pairs;
+    
 
     // construct empty message
     public Message() {
-        map = new HashMap<String, String>();
+        pairs = new ArrayList<String[]>();
     }
 
     private int checksumOf(String s) {
@@ -28,7 +29,7 @@ public class Message {
 
     // construct the original message from a string
     public Message(String s) {
-        map = new HashMap<String, String>();
+        pairs = new ArrayList<String[]>();
         Pattern p = new Pattern("^\{(\d)(.+)\|(.+)\}$");
         Matcher m = p.matcher(s);
         if(!m.matches() || m.groupCount() < 3)
@@ -46,15 +47,15 @@ public class Message {
         for(int i=0; i<split.length; i++) {
             int colon = split[i].indexOf(":");
             if(colon == -1)
-                map.put(split[i], "");
+                pairs.add(new String[]{split[i], null});
             else
-                map.put(split[i].substring(0,colon), split[i].substring(colon+1));
+                pairs.add(new String[]{split[i].substring(0, colon), split[i].substring(colon+1)});
         }
     }
 
     // return the underlying HashMap
-    public HashMap<String, String> getMap() {
-        return map;
+    public ArrayList<String[]> getMap() {
+        return pairs;
     }
 
     // return the sequence number
@@ -65,11 +66,12 @@ public class Message {
     // serialize message for transmission
     public String toString() {
         StringBuffer b = new StringBuffer("{");
-        Iterator<String> keys = map.keySet().iterator();
-        while(keys.hasNext()) {
-            String k = keys.next();
-            String v = map.get(k);
-            if(v.length() > 0) {
+        Iterator<String[]> pairsIterator= pairs.iterator();
+        while(pairsIterator.hasNext()) {
+            String[] pair = pairsIterator.next();
+            String k = pair[0];
+            String v = pair[1];
+            if(v != null) {
                 b.append(k);
                 b.append(':');
                 b.append(v);
@@ -82,3 +84,4 @@ public class Message {
         return b.toString();
     }
 }
+
