dc99bc56e1396fd63a9d2c86f596fddebe9df9a5
Neal Milstein <neal.milstein@gmail.com>
2013-04-22 18:46:06 -0400

    Added javadoc and comments for Claw, Movement, and Telemetry

--- lab7/Claw.java
+++ lab7/Claw.java
@@ -1,31 +1,54 @@
 import lejos.nxt.*;
 
+/**
+ * An interface for the claw peripheral.
+ */
 public class Claw {
 	
-	private int angle;
-	private NXTRegulatedMotor motor;
-	private MotorPort motorPort;
+	private int angle; // holds the current rotation angle of the claw
+	private NXTRegulatedMotor motor; // the NXT motor that manipulates the claw
+	private MotorPort motorPort; // the port for the NXT motor
 	
 	private static final int GEAR_RATIO = 3;
 	
+	/**
+	 * Creates a Claw object to interface with the claw
+	 * @param mport the port for the claw motor
+	 */
 	public Claw(MotorPort mport){
 		angle = 0;
 		motor = new NXTRegulatedMotor(mport);
 		motor.resetTachoCount();
 	}
 	
+	/**
+	 * Gets the current rotation angle of the claw
+	 * @return the angle of the claw
+	 */
 	public int getAngle(){
 		return angle;
 	}
 	
+	/**
+	 * Gets the port used by the claw motor
+	 * @return the port of the claw motor
+	 */
 	public MotorPort getMotorPort(){
 		return motorPort;
 	}
 	
+	/**
+	 * Sets the port for the claw motor to use
+	 * @param mport the given claw motor port
+	 */
 	public void setMotorPort(MotorPort mport){
 		motorPort = mport;
 	}
 	
+	/**
+	 * Rotates the claw by the given number of degrees (relative to present rotation)
+	 * @param degrees the given number of degrees to rotate by
+	 */
 	public void rotate(int degrees){
 		degrees %= 360; //anything over 360 is redundant
 		motor.rotate(degrees / GEAR_RATIO);
--- lab7/Movement.java
+++ lab7/Movement.java
@@ -1,7 +1,14 @@
 import lejos.nxt.*;
 import java.util.ArrayList;
 
+/**
+ * Interface for all of the motorized peripherals of the robot. Contains methods to read and
+ * maniuplate the left and right motor of the robot.
+ * 
+ */
 public class Movement {
+	
+	// NXT motors and their corresponding ports
 	
 	private NXTRegulatedMotor left_motor;
 	private MotorPort left_motor_port;
@@ -9,13 +16,21 @@ public class Movement {
 	private NXTRegulatedMotor right_motor;
 	private MotorPort right_motor_port;
 	
+	// The status of the left and right motors. Forward = 1, backward = -1
 	private int LEFT_ROTATION = 1;
 	private int RIGHT_ROTATION = 1;
 	
+	// starting motor speed
 	private static final int START_SPEED = 0;
+	
+	// max motor speed
 	private static final int MAX_SPEED = 128;
 	
-	
+	/**
+	 * Creates a new Movement class with the given left and right ports.
+	 * @param leftport the port to use for the left motor
+	 * @param rightport the port to use for the right motor
+	 */
 	public Movement(MotorPort leftport, MotorPort rightport){
 		left_motor = new NXTRegulatedMotor(leftport);
 		right_motor = new NXTRegulatedMotor(rightport);
@@ -26,52 +41,77 @@ public class Movement {
 		setRightMotorPort(rightport);
 	}
 	
+	/**
+	 * Sets the speed of the left and right motor. Negative speed values indicate backwards motion
+	 * @param left_speed speed of the left motor
+	 * @param right_speed speed of the right motor
+	 */
 	public void setSpeed(int left_speed, int right_speed){
 		
 		left_motor.setSpeed(left_speed);
 		right_motor.setSpeed(right_speed);
 		
-		if (left_speed >= 0 && right_speed >= 0){
+		if (left_speed >= 0) {
 			LEFT_ROTATION = 1;
-			RIGHT_ROTATION = 1;
 			left_motor.forward();
-			right_motor.forward();
-		}
-		else if (left_speed < 0 && right_speed >= 0){
+		} else {
 			LEFT_ROTATION = -1;
+			left_motor.backward();
+		}
+		
+		if (right_speed >= 0) {
 			RIGHT_ROTATION = 1;
-			left_motor.backward();
 			right_motor.forward();
-		}
-		else if (left_speed >= 0 && right_speed < 0){
-			LEFT_ROTATION = 1;
+		} else {
 			RIGHT_ROTATION = -1;
-			left_motor.forward();
-			right_motor.backward();
-		}
-		else if (left_speed < 0 && right_speed < 0){
-			LEFT_ROTATION = -1;
-			RIGHT_ROTATION = -1;
-			left_motor.backward();
 			right_motor.backward();
 		}
 	}
 	
+	/**
+	 * 
+	 * @return the speed of the left motor
+	 */
 	public int getLeftSpeed(){
 		return (int)(LEFT_ROTATION * left_motor.getSpeed());
 	}
+	
+	/**
+	 * 
+	 * @return the speed of the right motor
+	 */
 	public int getRightSpeed(){
 		return (int)(RIGHT_ROTATION * right_motor.getSpeed());
 	}
+	
+	/**
+	 * 
+	 * @return the port of the left motor
+	 */
 	public MotorPort getLeftMotorPort(){
 		return left_motor_port;
 	}
+	
+	/**
+	 * 
+	 * @return the port of the right motor
+	 */
 	public MotorPort getRightMotorPort(){
 		return right_motor_port;
 	}
+	
+	/**
+	 * Sets the port of the left motor
+	 * @param lport the new port for the left motor
+	 */
 	public void setLeftMotorPort(MotorPort lport){
 		left_motor_port = lport;
 	}
+	
+	/**
+	 * Sets the port of the right motor
+	 * @param rport the new port for the right motor
+	 */
 	public void setRightMotorPort(MotorPort rport){
 		right_motor_port = rport;
 	}
