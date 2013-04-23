88c512021bba1e7b162c82e1edbdde8ae536e75c
Neal Milstein <neal.milstein@gmail.com>
2013-04-22 22:37:00 -0400

    added comments for DebuggingTool Message and Connection

--- lab7/DebuggingTool/src/control/comm/Connection.java
+++ lab7/DebuggingTool/src/control/comm/Connection.java
@@ -1,36 +1,47 @@
 package control.comm;
 import lejos.pc.comm.*;
 
+/**
+ * Represents a connection to the NXT base station. Used for communication wiht the base station.
+ */
 public class Connection {
-  private NXTComm comm;
+  private NXTComm comm; // the private NXTComm object used for communication
 
+  /**
+   * Connect to the base station using the hard-coded base station address. Exits if a connection
+   * cannot be made.
+   * @return the NXTComm connection object
+   */
   private NXTComm connect()
   {
     comm = null;
+    // try to create a connection
     try
     {
       comm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
     }
     catch (NXTCommException e)
     {
-      
+      // uh oh! could not create it
       e.printStackTrace();
       System.err.println("Couldn't create NXT comm. Exiting.");
       System.exit(-1);
     }
     
-    // Connect
+    // base station parameters
     NXTInfo info = new NXTInfo();
     info.protocol = NXTCommFactory.BLUETOOTH;
     info.name = "LEAD7";
     info.deviceAddress = "00165313E6DB";
     
+    // try to connect
     try
     {
       comm.open(info, NXTComm.PACKET);
     }
     catch (NXTCommException e)
     {
+      // uh oh! couldn't connect
       e.printStackTrace();
       System.err.println("Couldn't connect to NXT " + info.name
           + ". Exiting.");
