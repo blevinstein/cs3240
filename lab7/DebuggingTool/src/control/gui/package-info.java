31362fbdd14a141e212656f24bc49bde749a042f
balexanderweaver <brian.alexander.weaver@gmail.com>
2013-04-22 23:51:54 -0300

    Name changes in package-info.java

--- lab7/DebuggingTool/src/control/gui/package-info.java
+++ lab7/DebuggingTool/src/control/gui/package-info.java
@@ -1,11 +1,11 @@
 /**
  * Package for all classes related to UI and graphics.
  * @version 1.0 - Build 04/01/2013
- * @author Stephanie Colen
- * @author Sarina Padilla
- * @author Hubert Chen
- * @author Andy Barron
- * @author John Zambrotta
+ * @author Bryan Walsh
+ * @author Brian Levinstein
+ * @author Neal Milstein
+ * @author Yuchen Zou
+ * @author Alex Weaver
  *
  */
 package control.gui;
